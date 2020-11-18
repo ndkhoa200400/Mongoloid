36842d56c536fe7964795be3a75d41322fafc2ee
levanthuanmy <levanthuanmy2000@gmail.com>
2020-11-18 13:22:10 +0700

    xong trang thong tin shop

---
+++ pages/shop-channel/shop-information/shop-infor.js
@@ -0,0 +1,60 @@
+function saveName() {
+    const getTagName = document.getElementById("shop-name");
+    const switching  = document.getElementById("shop-name-active");
+    getTagName.hidden="on";
+    switching.hidden="";
+}
+function savePhone() {
+    const getTagPhone = document.getElementById("shop-phone");
+    const switching = document.getElementById("shop-phone-active");
+    getTagPhone.hidden="on";
+    switching.hidden="";
+}
+function saveAddress() {
+    const getTagAdd = document.getElementById("shop-address");
+    const switching = document.getElementById("shop-address-active");
+    getTagAdd.hidden="on";
+    switching.hidden="";
+}
+function saveDay() {
+    const getTagDay = document.getElementById("shop-day");
+    const switching = document.getElementById("shop-day-active");
+    getTagDay.hidden="on";
+    switching.hidden="";
+}
+function saveDescribe() {
+    const getTagDescribe = document.getElementById("shop-describe");
+    const switching = document.getElementById("shop-describe-active");
+    getTagDescribe.hidden="on";
+    switching.hidden="";
+}
+function modifyName() {
+    const getTagName = document.getElementById("shop-name");
+    const switching  = document.getElementById("shop-name-active");
+    getTagName.hidden="";
+    switching.hidden="on";
+}
+function modifyPhone() {
+    const getTagPhone = document.getElementById("shop-phone");
+    const switching = document.getElementById("shop-phone-active");
+    getTagPhone.hidden="";
+    switching.hidden="on";
+}
+function modifyAddress() {
+    const getTagAdd = document.getElementById("shop-address");
+    const switching = document.getElementById("shop-address-active");
+    getTagAdd.hidden="";
+    switching.hidden="on";
+}
+function modifyDay() {
+    const getTagDay = document.getElementById("shop-day");
+    const switching = document.getElementById("shop-day-active");
+    getTagDay.hidden="";
+    switching.hidden="on";
+}
+function modifyDescribe() {
+    const getTagDescribe = document.getElementById("shop-describe");
+    const switching = document.getElementById("shop-describe-active");
+    getTagDescribe.hidden="";
+    switching.hidden="on";
+}
